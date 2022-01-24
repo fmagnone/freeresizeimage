4f9d81142d5adbd19a23f949fbd42db39c7893be
fmagnone <fmagnone@gmail.com>
2022-01-23 22:57:30 -0300

    General developmen

---
+++ app.py
@@ -0,0 +1,89 @@
+#### #### #### #### #### ####
+# Profile Image Resize - 2022
+
+# Import Librarys
+import os
+
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask_cors import CORS, cross_origin
+# from werkzeug import secure_filename
+
+# Global Variables
+UPLOAD_FOLDER = '/static/uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+# Configure application
+app = Flask(__name__)
+CORS(app, support_credentials=True)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+
+    response.headers["Access-Control-Expose-Headers"] = "Content-Disposition, Content-Length, X-Content-Transfer-Id"
+    #response.headers["Content-Type"] = "image/jpeg"
+    #response.headers["Content-Length"] = "3965123"
+    #response.headers["Content-Disposition"] = "inline; filename='my-file.jpg'"
+    #response.headers["X-Content-Transfer-Id"] = "12345"
+
+    return response
+
+# Index Page
+
+
+@app.route("/", methods=["GET", "POST"])
+@cross_origin(supports_credentials=True)
+def index():
+    if request.method == "POST":
+        # POST method
+        print("post method called")
+
+        return redirect("/")
+
+    else:
+        # GET method
+
+        return render_template('index.html')
+
+# Upload?
+
+
+@app.route("/static/uploads", methods=["POST"])
+@cross_origin(supports_credentials=True)
+def upload():
+    print()
+    return redirect("/")
+
+
+@app.route('/uploader', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save("static/uploads/" + f.filename)
+        return 'file uploaded successfully'
+
+
+# Contact Page
+@app.route("/contact")
+def contact_page():
+    return render_template('contact.html')
+
+
+@app.route("/filesave", methods=['POST'])
+def list_files():
+    f = request.files['file']
+    f.save("static/uploads/" + f.filename)
+    return jsonify(files)
+
+
+# Debugger mode
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/js/gen.js
@@ -0,0 +1,291 @@
+/*!
+ * Free Resize Image - Copyright 2022
+ * --
+ * General Scripts
+ */
+
+// General variables
+var downloadName = "resized_unknown.jpg";
+var downloadURL = "";
+var uploadedImages = {};
+var img_width = 800;
+var img_height = 800;
+
+// Buttons Action Detection
+$(document).ready(function () {
+    // Image from File button or general box
+    $("#uploadBox").click(function () {
+        getManualUploadContents();
+    });
+    $("#btnUploadFromFile").click(function () {
+        getManualUploadContents();
+    });
+
+    // Image from URL button
+    $("#btnUploadFromUrl").click(function () {
+        getClipboardContents();
+    });
+});
+
+// Resolution Range change value
+/*
+document.getElementById('resolution-percentage').addEventListener('change', event => {
+    resolution = document.getElementById('resolution-percentage').value;
+    document.getElementById('resolution-percentage-label').innerHTML = "Image compression: " + resolution + "%";
+    //console.log("resolution change", resolution);
+})
+*/
+
+// Ctrl + V Detection
+$(document).ready(function () {
+    var ctrlDown = false,
+        ctrlKey = 17,
+        cmdKey = 91,
+        vKey = 86;
+
+    $(document)
+        .keydown(function (e) {
+            if (e.keyCode == ctrlKey || e.keyCode == cmdKey) ctrlDown = true;
+        })
+        .keyup(function (e) {
+            if (e.keyCode == ctrlKey || e.keyCode == cmdKey) ctrlDown = false;
+        });
+
+    $(".no-copy-paste").keydown(function (e) {
+        if (ctrlDown && e.keyCode == vKey) return false;
+    });
+
+    $(document).keydown(function (e) {
+        if (ctrlDown && e.keyCode == vKey) {
+            getClipboardContents();
+        }
+    });
+});
+
+// Alert Message
+var alertPlaceholder = document.getElementById("liveAlertPlaceholder");
+var duration = 5000;
+function alert_message(message, type) {
+    var wrapper = document.createElement("div");
+    wrapper.innerHTML =
+        '<div class="alert alert-' +
+        type +
+        ' alert-dismissible" role="alert">' +
+        message +
+        '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>';
+
+    if (alertPlaceholder) {
+        alertPlaceholder.append(wrapper);
+        setTimeout(function () {
+            wrapper.parentNode.removeChild(wrapper);
+        }, duration);
+    }
+}
+
+// Get Contents Manual Upload
+function getManualUploadContents() {
+    $("#inputUploadFromFile").trigger("click");
+
+    // Select File
+    const inputElement = document.getElementById("inputUploadFromFile");
+    inputElement.addEventListener("change", handleFiles, false);
+    function handleFiles() {
+        const fileList = this.files;
+
+        // Get file information
+        const uploaded_files_count = fileList.length;
+        let uploaded_files_names = [];
+        console.log("Files uploaded: ", uploaded_files_count);
+        for (
+            let i = 0, uploaded_files_count = fileList.length;
+            i < uploaded_files_count;
+            i++
+        ) {
+            const file = fileList[i];
+            uploaded_files_names.push(file.name);
+            console.log("File " + i + " name = " + file.name);
+        }
+        alert_message(
+            "We catch " +
+            uploaded_files_count +
+            " file(s) dopped with this info:<br/>" +
+            uploaded_files_names,
+            "success"
+        );
+    }
+}
+
+// Get Contents Clipboard Upload
+async function getClipboardContents() {
+    let pasted_data = "(no data)";
+
+    try {
+        // Read image data
+        const img = await navigator.clipboard.read();
+
+        // Read text data
+        const text = await navigator.clipboard.readText();
+        pasted_data = text;
+
+        console.log("Pasted content: ", img);
+        console.log("Pasted content: ", text);
+
+        if (text != "") {
+            alert_message(
+                "We catch some text from Ctrl+V with this info:<br/>" + pasted_data,
+                "success"
+            );
+        } else {
+            alert_message(
+                "We catch something with Ctrl+V but is not an URL... Please copy your URL and try again!",
+                "danger"
+            );
+        }
+    } catch (err) {
+        console.error("Failed to read clipboard contents: ", err);
+        alert_message(
+            "Sorry, in this case we are not allowed to catch the data, please try another method!",
+            "danger"
+        );
+    }
+}
+
+// Get Content Drag and Drop - FilePond Plugin
+FilePond.registerPlugin(
+    //FilePondPluginGetFile,
+    FilePondPluginImagePreview,
+    FilePondPluginImageResize,
+    FilePondPluginImageTransform,
+);
+
+// Append Pond to document
+const input = document.querySelector('input[id="filePondUpload"]');
+
+// Create a FilePond instance and post files to /upload
+const pond = FilePond.create(input, {
+    name: 'filepond',
+    maxFiles: 1,
+    labelIdle: 'Drag & drop your image<br/><span class="filepond--label-action">or browse to upload</span>',
+    server: {
+        url: "http://127.0.0.1:5000/",
+        timeout: 7000,
+        process: {
+            url: "./",
+            method: "POST",
+            withCredentials: true,
+            headers: {},
+            timeout: 7000,
+            onload: null,
+            onerror: null,
+            ondata: null,
+        },
+        revert: {
+            url: "./",
+            method: "POST",
+            //withCredentials: true,
+            headers: {},
+        },
+    },
+
+    // Call back when image is added
+    onaddfile: (err, fileItem) => {
+        //console.log("On Add File Function called");
+        //console.log(err, fileItem.getMetadata('resize'));
+    },
+
+    // onpreparefile(file, output)
+    // File has been transformed by the transform plugin or another plugin
+    // subscribing to the prepare_output filter. It receives the file item and the output data.
+    onpreparefile: (file, output) => {
+        console.log("On Prepare File Function called");
+
+        // Create new image
+        const img = new Image();
+        img.src = URL.createObjectURL(output);
+        console.log(file.fileSize, output.size);
+
+        // Pass new image URL
+        downloadName = "res_" + file.filename;
+        downloadURL = img.src;
+
+        // Pass new image to dict
+        uploadedImages[downloadName] = downloadURL;
+
+        // Console print
+        //console.log("Image Name: ", downloadName);
+        //console.log("Image URL: ", downloadURL);
+
+        //console.log(file);
+
+        console.log(uploadedImages);
+
+
+        // Disabel FilePond Label
+        let label = document.getElementsByClassName("filepond--drop-label")[0];
+        label.style.display = "none";
+    },
+
+    // Resize the file
+    //imageResizeUpscale: false,
+    allowImageResize: true,
+    imageResizeMode: "contain",
+    imageResizeTargetWidth: 400,
+    imageResizeTargetHeight: 400,
+
+    /*
+        imageTransformVariants: {
+            thumb_medium_: (transforms) => {
+              transforms.resize.size.width = 384;
+              return transforms;
+            },
+            thumb_small_: (transforms) => {
+              transforms.resize.size.width = 128;
+              return transforms;
+            }
+        },
+        */
+
+    onremovefile: function (error, file) {
+        console.log("On Remove File Function called");
+        if (file.serverId) {
+            //let input = document.createElement('input');
+            //input.type = 'hidden';
+            //input.name = 'DeletedFilepondImages';
+            //input.value = file.serverId;
+            //uploadForm.appendChild(input);
+        }
+
+        // Disabel FilePond Label
+        let label = document.getElementsByClassName("filepond--drop-label")[0];
+        label.style.display = "flex";
+    },
+    onaddfileprogress(file, progress) {
+        console.log("On Add File Progress?", progress);
+        //buttonForm.classList.remove('filepondUpload');
+        //buttonForm.removeAttribute('disabled');
+    },
+
+    // Call when upload finishes
+    onprocessfile(error, file) {
+        console.log("File Uploaded: ", file);
+        //buttonForm.classList.remove('filepondUpload');
+        //buttonForm.removeAttribute('disabled');
+    },
+    onprocessfiles() {
+        console.log("All functions finished");
+
+        enableDownloadBtn();
+    },
+});
+
+
+// Enable Download Button
+function enableDownloadBtn(fileUrl) {
+    var downloadBtn = document.getElementById("downloadFileBtn");
+    downloadBtn.classList.remove("disabled");
+    //downloadBtn.innerHTML = "Download file: " + downloadName;
+    downloadBtn.href = downloadURL;
+    downloadBtn.setAttribute("download", downloadName);
+
+    //downloadBtn.click();
+}
