a2d13a91db6c714ad6877768829a6ecd95672bab
fmagnone <fmagnone@gmail.com>
2022-11-01 23:50:42 +0100

    Include Adds

--- app.py
+++ app.py
@@ -104,6 +104,9 @@ def sitemap_xml():
 def robots_txt():
     return send_from_directory(os.path.join(app.root_path, 'static'),'robots.txt',mimetype='text/plain')
 
+@app.route('/ads.txt')
+def robots_txt():
+    return send_from_directory(os.path.join(app.root_path, 'static'),'ads.txt',mimetype='text/plain')
 
 # Favicon
 @app.route('/favicon.ico')
